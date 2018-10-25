a4f7b35fe21ede8115771407f0ba25831b022762
leonardo <leonardo.dossantos@unige.ch>
2018-10-25 16:13:33 +0200

    First commit of the plotting routine

---
+++ climax/PlotTools.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+
+
+def multiline(xs, ys, c, ax=None, **kwargs):
+    """
+    Plot lines with different colorings
+
+    Args:
+        xs: iterable container of x coordinates
+        ys: iterable container of y coordinates
+        c: iterable container of numbers mapped to colormap
+        ax (optional): Axes to plot on.
+        **kwargs (optional): passed to LineCollection
+
+    Returns:
+        lc : LineCollection instance.
+    """
+
+    # find axes
+    ax = plt.gca() if ax is None else ax
+
+    # Ensure every passed values are ``numpy.array``
+    xs = np.array(xs)
+    ys = np.array(ys)
+    c = np.array(c)
+
+    # Check if ``xs`` has the same shape as ``ys``. If not, assume all ``ys``
+    # will share the same ``xs``
+    if np.shape(xs) != np.shape(ys):
+        xs = np.array([xs for i in range(len(ys))])
+    else:
+        pass
+    print(np.shape(xs))
+
+    # create LineCollection
+    segments = [np.column_stack([x, y]) for x, y in zip(xs, ys)]
+    lc = LineCollection(segments, **kwargs)
+
+    # set coloring of line segments
+    lc.set_array(np.asarray(c))
+
+    # add lines to axes and rescale
+    ax.add_collection(lc)
+    ax.autoscale()
+    return lc
